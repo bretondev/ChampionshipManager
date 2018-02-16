52d6f9dd48b2bab9fccb4e1c95fdcfcc306e67cd
bretondev <bretondev@gmail.com>
2018-02-16 11:02:13 +0100

    Utilisation de Spring Cache

--- ChampionshipManager/src/org/bretondev/championshipmanager/service/impl/ChampionshipServiceImpl.java
+++ ChampionshipManager/src/org/bretondev/championshipmanager/service/impl/ChampionshipServiceImpl.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.bretondev.championshipmanager.dao.ChampionshipDAO;
 import org.bretondev.championshipmanager.entities.Championship;
 import org.bretondev.championshipmanager.service.ChampionshipService;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -19,24 +21,28 @@ public class ChampionshipServiceImpl implements ChampionshipService{
  
     @Override
     @Transactional
+    @Cacheable(value="dataCache", key="'listChampionships'")
     public List<Championship> listChampionships() {
         return this.championshipDAO.listChampionships();
     }
  
     @Override
     @Transactional
+    @CacheEvict(value="dataCache", key="'listChampionships'")
     public void createChampionship(Championship c) {
         this.championshipDAO.createChampionship(c);
     }
     
     @Override
     @Transactional
+    @CacheEvict(value="dataCache", key="'listChampionships'")
     public void deleteChampionship(Integer id) {
         this.championshipDAO.deleteChampionship(id);
     }
     
     @Override
     @Transactional
+    @CacheEvict(value="dataCache", key="'listChampionships'")
     public void updateChampionship(Championship c) {
         this.championshipDAO.updateChampionship(c);
     }
